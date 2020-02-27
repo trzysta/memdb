19638c7c3a5c1e9c3507d854fb622ce5a1d84adf
trzysta <61110842+trzysta@users.noreply.github.com>
2020-02-27 13:19:54 +0100

    Update wydatki.js

--- wydatki.js
+++ wydatki.js
@@ -9,7 +9,7 @@ function budgetAllocationLeft( entrySpend ) {
   var s = Math.abs( entrySpend.field(S_FIELD_AMOUNT) );
   var budgetLinks = entrySpend.field(S_FIELD_BUDGET_LINK);
   for ( i=0; budgetLinks.length; i++  ) {
-    s -= budgetLinks[i].attr(S_FIELD_BUDGET_LINK_AMOUNT);
+    s -= budgetLinks[i].attr( S_FIELD_BUDGET_LINK_AMOUNT );
   }
   return s;
 
