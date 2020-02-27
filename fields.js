6ea32ad89ead2eee0888039a60098c7050293bb6
trzysta <61110842+trzysta@users.noreply.github.com>
2020-02-27 13:02:04 +0100

    a

--- fields.js
+++ fields.js
@@ -84,16 +84,3 @@ const B_MSG_BUDGET_FOUND = "...znaleziono budżet: ";
 // ***** baza KALENDARZ ZADAŃ
 const A_FIELD_MONTH = "Miesiąc";
 const A_FIELD_DATES = "Daty";
-
-// zmienne
-var arrMonths_pl  = ["styczeń", "luty", "marzec", "kwiecien", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "listopad", "grudzień"];
-var arrEditors    = ["MalgorzataG", "ElzbietaZ", "trzystaIZABELA", "trzysta"];
-var arrManagers   = ["trzysta"];
-var arrNames      = ["Małgorzata Grabowska", "Elżbieta Zdziech", "Izabela Skowrońska", "Marceli Matynia"];
-var libWyplaty    = libByName("Wypłaty");
-//var libWydatki    = libByName("Wydatki");
-var libBudzet     = libByName("Budżet");
-var libOsiedla    = libByName("Osiedla");
-var libKontrole   = libByName("Kontrole");
-var libZadania    = libByName("Kalendarz zadań");
-var withdrawalMaker = "Marceli Matynia";
