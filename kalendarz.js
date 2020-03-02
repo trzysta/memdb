a9af29738150d5628a1a57925a5840a4415f7b79
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-02 21:22:48 +0100

    Update kalendarz.js

--- kalendarz.js
+++ kalendarz.js
@@ -20,7 +20,7 @@ function selectDays( entryAction, dayWeekNumber ) {
       for ( var i=1; i <= 31; i++) {
         entryAction.set(i, false);
         arrDates = new Array;
-        arrDays = new Array;  
+        arrDays = new Array;
       };
       break;
     default:
@@ -55,7 +55,7 @@ function displayEntryName( entryCalendar ) {
      case C_FIELD_TYPE_VALUE_TEMPLATE:
         o +=  entryCalendar.field(C_FIELD_ACTION_DOMAIN) + VIEW_SEP +
               entryCalendar.field(C_FIELD_ACTION) + VIEW_SEP +
-              entryCalendar.field(C_FIELD_CONTRACT);
+              entryCalendar.field(C_FIELD_CONTRACT)[0];
        break;
      default:
         o +=  moment(entryCalendar.field(C_FIELD_MONTH)).format('YYYY-MM') + VIEW_SEP +
