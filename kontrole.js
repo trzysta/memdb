f09064dbb5740e7b799c876a42b855a5db31ad0f
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 13:21:34 +0100

    Update kontrole.js

--- kontrole.js
+++ kontrole.js
@@ -1,5 +1,20 @@
 
 
 function getCheckpointLabel( fieldCount ) {
-  return C_FIELD_CHECKPOINT_LABELS[fieldCount];
+  return R_FIELD_CHECKPOINT_LABELS[fieldCount];
+}
+
+
+
+
+
+
+
+
+
+function updateControlForm_linkContract( entryContract, entryControl ) {
+
+  var arrBuildings = entryContract.field(P_FIELD_BUILDINGS).split("\n");
+  entryControl.set(R_FIELD_BUILDING, arrBuildings)
+
 }
