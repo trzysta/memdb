0a3bcb081dee44223bb1a7c5dadc4032118650ca
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 17:22:07 +0100

    Update kontrole.js

--- kontrole.js
+++ kontrole.js
@@ -18,14 +18,22 @@ function saveFirstTime( entryControl ) {
   var m = arrMonths_pl[ parseInt(moment().format('M'))-1 ];
   var y = moment().format('YYYY');
   var t = entryControl.field(R_CONTRACT_LINK)[0].field(P_FIELD_TAG);
+  var arrSearchResult = new Array();
 
+  // szukanie czynności codziennie i kilka razy w tygodniu
+  for (f=0; f < R_FREQ_LINKED_WEEK.length; f++) {
+    query = t + " " + R_FREQ_LINKED_WEEK[f];
+    arrSearchResult.push( libZadania.find(query) )
+  }
+
+  // szukanie czynności z tego miesiąca
   query = t + " " + m + " " + y;
-  var arrSearchResult = libZadania.find(query);
-  message (query + " " +  arrSearchResult.length);
+  arrSearchResult.push( libZadania.find(query) )
 
   for (i=0; i < arrSearchResult.length; i++) {
     var entryAction = arrSearchResult[i];
     entryControl.link( R_FIELD_ACTION_LINK, entryAction );
   }
+  
   entryControl.show();
 }
