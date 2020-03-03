ce221feab87f2dc3d77ec45e159e56bfa53f6bde
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 16:47:31 +0100

    a

--- kontrole.js
+++ kontrole.js
@@ -5,28 +5,26 @@ function getCheckpointLabel( fieldCount ) {
 }
 
 
-
 function createControl( entryControl ) {
   entryControl.set(FIELD_IS_NEW, true);
 }
 
 
 function saveFirstTime( entryControl ) {
+
   entryControl.set(FIELD_IS_NEW, false);
-  getActionsThisMonth (entryControl);
+
+  var m = arrMonths_pl[ parseInt(moment().format('M'))-1 ];
+  var y = moment().format('YYYY');
+  var t = entryControl.field(R_CONTRACT_LINK)[0].field(P_FIELD_TAG);
+
+  query = t + " " + m + " " + y;
+  var arrSearchResult = libZadania.find(query);
+
+  for (i=0; i < arrSearchResult.length; i++) {
+    var entryAction = arrSearchResult[i];
+    entryControl.link( R_FIELD_ACTIONS_LINK, entryAction );
+  }
+
   entryControl.show();
-}
-
-
-
-function getActionsThisMonth ( entryControl ) {
-    var m = arrMonths_pl[ parseInt(moment().format('M'))-1 ];
-    var y = moment().format('YYYY');
-    var t = entryControl.field(R_CONTRACT_LINK)[0].field(P_FIELD_TAG);
-    query = t + " " + m + " " + y;
-    var arrSearchResult = libZadania.find(query);
-    for (i=0; i < arrSearchResult.length; i++) {
-      entryAction = arrSearchResult[i];
-      entryControl.link( R_FIELD_ACTIONS_LINK, entryAction );
-    }
 }
