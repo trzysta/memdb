7d9d1959687744d22825fa96f46906adc90655c3
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 12:43:31 +0100

    Update kontrole.js

--- kontrole.js
+++ kontrole.js
@@ -1,18 +1,3 @@
-/*
-var me = masterEntry();
-var e = entry();
-var nSaldo = 0;
-var lim = e.field("Limit");
-
-nSaldo = e.field("Saldo") + Math.abs(me.field("Kwota"));
-
-e.set("Saldo", nSaldo);
-e.set("Zostało", lim - nSaldo );
-*/
-
-
-function getActionsForInspection ( entryInspection, entryAction ) {
-}
 
 
 function getCheckpointLabel( fieldCount ) {
