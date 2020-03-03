d83cffbb17a67250622a7e0113c9ef9ccb1132c9
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 15:57:38 +0100

    Update kontrole.js

--- kontrole.js
+++ kontrole.js
@@ -5,14 +5,6 @@ function getCheckpointLabel( fieldCount ) {
 }
 
 
-
-
-
-
-
-
-
-
 function getActionsThisMonth ( entryControl ) {
 
     var m = arrMonths_pl[ parseInt(moment().format('M'))-1 ];
@@ -20,18 +12,11 @@ function getActionsThisMonth ( entryControl ) {
     var t = entryControl.field(R_CONTRACT_LINK).field(P_FIELD_TAG);
 
     query = t + " " + m + " " + y;
+    message (query);
+    
     var arrSearchResult = libZadania.find(query);
     for (i=0; i < arrSearchResult.length; i++) {
       var entryAction = arrSearchResult[i];
       entryControl.link(R_FIELD_ACTIONS_LINK, entryAction );
     }
-}
-
-
-
-function updateControlForm_linkContract( entryContract, entryControl ) {
-
-  var arrBuildings = ["abc", "def"];//entryContract.field(P_FIELD_BUILDINGS).split("\n");
-  entryControl.set(R_FIELD_BUILDING, arrBuildings);
-
 }
