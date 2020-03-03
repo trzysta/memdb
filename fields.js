44f62dcbe88cf0b8de6ce85b642e8c6b89a1b56d
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-03 17:05:57 +0100

    a

--- fields.js
+++ fields.js
@@ -3,15 +3,7 @@ var arrMonths_pl  = ["styczeń", "luty", "marzec", "kwiecien", "maj", "czerwiec"
 var arrEditors    = ["MalgorzataG", "ElzbietaZ", "trzystaIZABELA", "trzysta"];
 var arrManagers   = ["trzysta"];
 var arrNames      = ["Małgorzata Grabowska", "Elżbieta Zdziech", "Izabela Skowrońska", "Marceli Matynia"];
-var libWyplaty    = libByName("Wypłaty");
-var libWydatki    = libByName("Wydatki");
-var libBudzet     = libByName("Budżet");
-var libOsiedla    = libByName("Osiedla");
-var libKontrole   = libByName("Kontrole");
-var libZadania    = libByName("Kalendarz zadań");
 var withdrawalMaker = "Marceli Matynia";
-
-
 
 // wspólne
 var FIELD_EDITOR = "Editor";
---
+++ libs.js
@@ -0,0 +1,6 @@
+var libWyplaty    = libByName("Wypłaty");
+var libWydatki    = libByName("Wydatki");
+var libBudzet     = libByName("Budżet");
+var libOsiedla    = libByName("Osiedla");
+var libKontrole   = libByName("Kontrole");
+var libZadania    = libByName("Kalendarz zadań");
