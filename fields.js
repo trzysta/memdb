a8490fcb5f1eaa001b8f1e2607b0a8956461b8df
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-04 20:47:17 +0100

    p

--- fields.js
+++ fields.js
@@ -5,6 +5,7 @@ var arrEditors    = ["MalgorzataG", "ElzbietaZ", "trzystaIZABELA", "trzysta"];
 var arrManagers   = ["trzysta"];
 var arrNames      = ["Małgorzata Grabowska", "Elżbieta Zdziech", "Izabela Skowrońska", "Marceli Matynia"];
 var withdrawalMaker = "Marceli Matynia";
+var LINE = "--------------------------------------------------";
 
 // wspólne
 var FIELD_EDITOR = "Editor";
