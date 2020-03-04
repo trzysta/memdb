75c037985d8fa861a1ca4b79b76e65dcdd367099
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-04 11:46:36 +0100

    Update kontrole.js

--- kontrole.js
+++ kontrole.js
@@ -25,13 +25,13 @@ function saveFirstTime( entryControl ) {
      for (j=0; j < C_FIELD_ACTION_DOMAIN_VALUES.length; j++) {
         val = C_FIELD_ACTION_DOMAIN_VALUES[j];
         if   (( entryAction.field(C_FIELD_ACTION_DOMAIN) == val ) &&
-              ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field() ) >= 0 ) &&
-              ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field() ) <= 5 )) {
+              ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field(R_FIELD_FREQENCY) ) >= 0 ) &&
+              ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field(R_FIELD_FREQENCY) ) <= 5 )) {
              // czynności wnetrza częste lub równe raz na tydzień
              actionsDaily.push(entryAction);
          }
          else if (( entryAction.field(C_FIELD_ACTION_DOMAIN) == val ) &&
-                  ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field()) > 5 )) {
+                  ( R_FIELD_FREQENCY_VALUES.indexOf( entryAction.field(R_FIELD_FREQENCY)) > 5 )) {
              actionsMonthly.push(entryAction);
          }
       }
