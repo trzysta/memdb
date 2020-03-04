74ab9d0a0f1f72a48531b964cbbe7fd8f4a05542
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-04 11:33:13 +0100

    Update fields.js

--- fields.js
+++ fields.js
@@ -1,3 +1,4 @@
+
 // zmienne
 var arrMonths_pl  = ["styczeń", "luty", "marzec", "kwiecien", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "listopad", "grudzień"];
 var arrEditors    = ["MalgorzataG", "ElzbietaZ", "trzystaIZABELA", "trzysta"];
@@ -21,59 +22,59 @@ var VALUE_DAYNUMBER_SUNDAY    = 7;
 var VIEW_SEP = " >>> ";
 
 // ***** baza wydatki
-var S_FIELD_IMPORTED = "Rozliczony";
-var S_FIELD_AMOUNT = "Kwota";
-var S_FIELD_DATE = "Data transakcji";
-var S_FIELD_TYPE = "Typ transakcji";
-var S_FIELD_TYPE_VALUE_EMPLOYEE_WITHDRAWAL = "Rozliczenie przelewem z pracownikiem"; // wartość pola
+var S_FIELD_IMPORTED            = "Rozliczony";
+var S_FIELD_AMOUNT              = "Kwota";
+var S_FIELD_DATE                = "Data transakcji";
+var S_FIELD_TYPE                = "Typ transakcji";
+var S_FIELD_TYPE_VALUE_EMPLOYEE_WITHDRAWAL = "Rozliczenie przelewem z pracownikiem";
 var S_FIELD_TYPE_VALUE_EMPLOYEE_CASH = "Rozliczenie gotówką z pracownikiem";
 var S_FIELD_TYPE_VALUE_ADVANCEPAYMENT_CASH = "Wypłacona gotówką zaliczka";
 var S_FIELD_TYPE_VALUE_ADVANCEPAYMENT_WITHDRAWAL = "Wypłacona przelewem zaliczka";
-var S_FIELD_BUDGET_LINK = "Budżet";
-var S_FIELD_BUDGET_LINK_AMOUNT = "Kwota";
+var S_FIELD_BUDGET_LINK         = "Budżet";
+var S_FIELD_BUDGET_LINK_AMOUNT  = "Kwota";
 
 var S_FIELD_COST_ALLOCATION_LINK = "Robicie kosztu na osiedla";
 var S_FIELD_COST_ALLOCATION_LINK_AMOUNT = "Kwota";
 
-var S_FIELD_CREATOR = "Dokonujący transakcji";
-var S_FIELD_CATEGORY = "Kategoria";
-var S_FIELD_RECIPIENT = "Przekazano osobie";
-var S_FIELD_SUPPLIER = "Dostawca";
-var S_FIELD_EMPLOYEE_LINK = "Pracownik";
-var S_FIELD_DESCRIPTION = "Opis";
-var S_FIELD_INVOICE_IMG = "Faktura";
-var S_FIELD_IS_FIELD_PAYED = "Do zapłaty";
-var S_FIELD_IS_FIELD_REINVOICE = "Czy wydatek należy refakturować";
+var S_FIELD_CREATOR             = "Dokonujący transakcji";
+var S_FIELD_CATEGORY            = "Kategoria";
+var S_FIELD_RECIPIENT           = "Przekazano osobie";
+var S_FIELD_SUPPLIER            = "Dostawca";
+var S_FIELD_EMPLOYEE_LINK       = "Pracownik";
+var S_FIELD_DESCRIPTION         = "Opis";
+var S_FIELD_INVOICE_IMG         = "Faktura";
+var S_FIELD_IS_FIELD_PAYED      = "Do zapłaty";
+var S_FIELD_IS_FIELD_REINVOICE  = "Czy wydatek należy refakturować";
 var S_FIELD_REINVOICE_CONTRACT_LINK = "Osiedle do faktury";
-var S_FIELD_NR = "Nr";
-var S_FIELD_NR_ASSIGNED = "Nr powiązany";
+var S_FIELD_NR                  = "Nr";
+var S_FIELD_NR_ASSIGNED         = "Nr powiązany";
 var S_FIELD_DESCRIPTION_DISPLAY = "Opis";
 var S_FIELD_COST_ALLOCATION_LEFT = "Kwota pozostała do rozpisania";
 
 // ***** baza WYPŁATY
-var P_FIELD_CLOSED = "Rozliczony";
-var P_FIELD_CLOSED_VALUE_YES  = "Rozliczony";
-var P_FIELD_CLOSED_VALUE_NO   = "W trakcie rozliczania";
-var P_FIELD_CASH_AMOUNT = "Wypłacono w gotówce";
-var P_FIELD_CASH_DATE = "Data wypłaty gotówki";
-var P_FIELD_WITHDRAWAL_AMOUNT = "Wpłacono na konto";
-var P_FIELD_WITHDRAWAL_DATE = "Data przelewu";
-var P_FIELD_EMPLOYEE_LINK = "Pracownik";
-var P_SPEND_LINK = "Wydatek";
-var P_FIELD_DESCRIPTION = "Uwagi";
-var P_FIELD_MONTH = "Miesiąc";
-var P_FIELD_PAYER = "Dokonujący wypłaty";
-var P_FIELD_ADVANCE_PAYMENT = "Zaliczki";
-var P_FIELD_WEEKENDS = "Dni wolne";
+var P_FIELD_CLOSED              = "Rozliczony";
+var P_FIELD_CLOSED_VALUE_YES    = "Rozliczony";
+var P_FIELD_CLOSED_VALUE_NO     = "W trakcie rozliczania";
+var P_FIELD_CASH_AMOUNT         = "Wypłacono w gotówce";
+var P_FIELD_CASH_DATE           = "Data wypłaty gotówki";
+var P_FIELD_WITHDRAWAL_AMOUNT   = "Wpłacono na konto";
+var P_FIELD_WITHDRAWAL_DATE     = "Data przelewu";
+var P_FIELD_EMPLOYEE_LINK       = "Pracownik";
+var P_SPEND_LINK                = "Wydatek";
+var P_FIELD_DESCRIPTION         = "Uwagi";
+var P_FIELD_MONTH               = "Miesiąc";
+var P_FIELD_PAYER               = "Dokonujący wypłaty";
+var P_FIELD_ADVANCE_PAYMENT     = "Zaliczki";
+var P_FIELD_WEEKENDS            = "Dni wolne";
 
 var P_ADD_DESCRIPTION_WITHDRAWAL = " wypłata przelewem za ";
-var P_ADD_DESCRIPTION_CASH = " wypłata gotówki za ";
+var P_ADD_DESCRIPTION_CASH      = " wypłata gotówki za ";
 
-var P_MSG_CLOSED_OR_NOACCESS = "Wpis już rozliczony lub nie masz uprawnień do zamknięcia rozliczenia";
-var P_MSG_NO_AMONT = "Uzupełnij kwoty wypłat, gotówka lub przelew. Jeśli rozliczenie jest bez wypłaty w polach kwot wstaw zero";
-var P_MSG_CLOSING = "Zamykam rozliczenie: ";
-var P_MSG_ADVANCE_PAYMENT = "Szukam zaliczek... ";
-var P_MSG_VALIDATION_ERR = "Nie można zapisać, popraw następujące błędy:";
+var P_MSG_CLOSED_OR_NOACCESS    = "Wpis już rozliczony lub nie masz uprawnień do zamknięcia rozliczenia";
+var P_MSG_NO_AMONT              = "Uzupełnij kwoty wypłat, gotówka lub przelew. Jeśli rozliczenie jest bez wypłaty w polach kwot wstaw zero";
+var P_MSG_CLOSING               = "Zamykam rozliczenie: ";
+var P_MSG_ADVANCE_PAYMENT       = "Szukam zaliczek... ";
+var P_MSG_VALIDATION_ERR        = "Nie można zapisać, popraw następujące błędy:";
 var P_MSG_VALIDATION_ERR_NO_WITHDRWAL = "- podaj datę i kwotę przelewu";
 var P_MSG_VALIDATION_ERR_NO_CASH = "- podaj datę i kwotę wypłaty gotówki";
 
@@ -113,29 +114,33 @@ var C_FIELD_ACTION_DOMAIN_VALUES  = ["Klatki","Garaż","Teren","Zieleń","Biuro"
 
 
 // ***** baza KONTROLA
-var R_FIELD_CHECKPOINT_LABELS = ["Posadzka, płytki, kamień, fugi czy są ślady po brudnym mopie, slady złego mycia przy rogach",
+var R_FIELD_CHECKPOINT_LABELS = ["Posadzka płytki kamień fugi czy są ślady po brudnym mopie slady złego mycia przy rogach",
                               "Zapach",
-                              "Parapety: kurz, martwe owady",
-                              "Przeszklenia, drzwi",
+                              "Parapety: kurz martwe owady",
+                              "Przeszklenia drzwi",
                               "Pajęczyny",
-                              "Kurz na włącznikach, elementach wystających",
-                              "Kurz na cokołach, brudne cokoły",
+                              "Kurz na włącznikach elementach wystających",
+                              "Kurz na cokołach brudne cokoły",
                               "Schody: spoczniki i podstopnice",
                               "Schody: zacieki na bokach",
-                              "Windy: stal, ślady, zacieki z oliwki",
+                              "Windy: stal ślady zacieki z oliwki",
                               "Windy: posadzka",
                               "Windy: lustra",
-                              "Barierki, poręcze" ];
+                              "Barierki poręcze" ];
+
 var R_FIELD_BUILDING        = "Budynek i klatka";
 var R_FIELD_CONTRACT_LINK   = "Osiedle";
 var R_FIELD_FREQENCY        = "Częstotliwość";
+
+/*  0-5  częstotliwość częściej niż raz miesiącu */
+/*  6-15 czstotliwość raz w miesiącu i rzadziej */
 var R_FIELD_FREQENCY_VALUES = [ "Codziennie",
                                 "1 raz w tygodniu",
                                 "2 razy w tygodniu",
                                 "3 razy w tygodniu",
                                 "4 razy w tygodniu",
-                                "1 raz na dwa tygodnie", /*  0-5  częstotliwość częściej niż raz miesiącu */
-                                "1 raz w miesiącu", /* 6-15 czstotliwość raz w miesiącu i rzadziej */
+                                "1 raz na dwa tygodnie",
+                                "1 raz w miesiącu",
                                 "2 razy w miesiącu",
                                 "3 razy w miesiącu",
                                 "4 razy w miesiącu",
