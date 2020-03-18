1e004c4ee722ad516bfd0534e1e26ae9ca639876
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-18 13:13:17 +0100

    a

--- osiedla.js
+++ osiedla.js
@@ -13,7 +13,7 @@ function linkContract ( targetLibraryName, entryContract, entryTarget ) {
         var tmp = "start";
 
         var dateStart = moment().startOf('month');
-        var dateEnd = moment();
+        var dateEnd = moment();     // sprawdzanie czynności od początku miesiąca do daty dziś
 
         for (let c = 0; c < allEntriesActionUnsorted.length; c++ ) {
           var sortOrder = allEntriesActionUnsorted[c].field("Sort");
@@ -28,7 +28,6 @@ function linkContract ( targetLibraryName, entryContract, entryTarget ) {
 
           if ( entryTarget.field(CON_FIELD_ACTION_DOMAIN).indexOf( entryAction.field(ACT_FIELD_ACTION_DOMAIN)) >= 0 ) {
              if ( actionDates.length <= 0 ) {                           // nie ma wpisanej daty oznacza że to czynność codzienna
-
                 entryTarget.link(CON_FIELD_ACTION_LINK, entryAction);
              } else {                                                   // jest wpisana data zatem sprawdzam czy data jest z tego miesiąca
                 for (let j=0; j < actionDates.length; j++ ) {
@@ -40,7 +39,7 @@ function linkContract ( targetLibraryName, entryContract, entryTarget ) {
           }
       };
       entryTarget.set(FIELD_IS_NEW, false);
-      //entryTarget.set("tmp", tmp);
+      entryTarget.set("tmp", tmp);
       entryTarget.show();
 
       break;
