938d9fc5a9818da2c5011f0e44be3077025913aa
trzysta <61110842+trzysta@users.noreply.github.com>
2020-03-18 16:00:23 +0100

    q

--- kontrole.js
+++ kontrole.js
@@ -111,6 +111,7 @@ function generateEmailBody ( entryControl ) {
 
   htmlBody = ""+
   "<p>Dzień dobry, to jest autoamtyczny e-mail zawierające informacje z wykonane kontroli.</p>" +
+
   "<p>Data i godzina kontroli:<b> "   + moment(entryControl.field(CON_FIELD_CONTROL_DATETIME)).format("YYYY-MM-DD hh:mm")  + "</b><br>"  +
   "Kontrolowane osiedle:<b> "         + entryControl.field(CON_FIELD_CONTRACT_LINK)[0].name + "</b><br>"  +
   "Kontrolowany obsza:<b> "            + entryControl.field(CON_FIELD_ACTION_DOMAIN).join(", ") + "</b><br>"  +
@@ -119,7 +120,7 @@ function generateEmailBody ( entryControl ) {
   htmlBody = htmlBody + "<p>Kontrola składa się z 2 części. Pierwsza część to kontrola wykonania czynności z umowy, sprawdzane są czynności które wg umowy powinny być wykonane częściej niż raz w tygodniu, a także czynności rzadziej wykonywane niż raz w tygodniu które zostały zaplanowane między początkiem miesiąca a datą bieżącą. <br> Drugą częścią kontroli jest sprawdzenie stanu czystości klatki.</p>" +
   "<p>Czynności z umowy które miały być wykonane od początku miesiąca do dnia kontroli zostały skontrolowane.</p>";
 
-  htmlBody = htmlBody + "<h2>Część pierwsza kontroli, sprawdzanie wykonania zadania wg umowy</h1>";
+  htmlBody = htmlBody + "<p><b>Część pierwsza kontroli, sprawdzanie wykonania zadania wg umowy</b><p>";
 
   for (let i = 0; i < entryControl.field(CON_FIELD_ACTION_LINK).length; i++ ) {
     htmlBody = htmlBody + "<span>" + (i+1) + " czynność: <b>" +  entryControl.field(CON_FIELD_ACTION_LINK)[i].field(ACT_FIELD_ACTION) + "</b>" +
