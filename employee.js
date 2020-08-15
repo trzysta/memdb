dabe662c93a0bdd57d472c67df509e728b205a12
trzysta <61110842+trzysta@users.noreply.github.com>
2020-08-15 10:54:40 +0200

    recalcHoliday

--- employee.js
+++ employee.js
@@ -2,25 +2,23 @@
 try {
 
   const Employee = function (e) {
-    log("employee.js::Employee: " + String(e));
+    log("employee.js::Employee: " + e.name);
 
     this.holidayUsed = 0;
     this.holidayTotal = 0;
     this.holidayLeft = 0;
+    this.entry = e;
 
-    if (e !== undefined) {
-      this.entry = e;
-      this.holidayUsed = this.entry.field(EMP_FIELD_HOLIDAY_USED);
-      this.holidayTotal = this.entry.field(EMP_FIELD_HOLIDAY_TOTAL);
-      this.holidayLeft = this.holidayTotal - this.holidayUsed;
-    }
+    this.holidayUsed = this.entry.field(EMP_FIELD_HOLIDAY_USED);
+    this.holidayTotal = this.entry.field(EMP_FIELD_HOLIDAY_TOTAL);
+    this.holidayLeft = this.holidayTotal - this.holidayUsed;
 
     this.recalcHoliday = function () {
-      log("recalcHoliday");
+      log("LOG recalcHoliday start");
 
       try {
         let libSalary = libByName(LIB_SALARIES_NAME);
-        let salaries = libSalary.linksTo(entry);
+        let salaries = libSalary.linksTo(e);
         let newHolidayUsed = 0;
 
         for (let i = 0; i <= salaries.length; i++) {
@@ -35,16 +33,14 @@ try {
         this.entry.set(EMP_FIELD_HOLIDAY_TOTAL, this.holidayTotal)
         this.entry.set(EMP_FIELD_HOLIDAY_LEFT, this.holidayLeft)
         message(EMP_MSG_HOLIDAY_RECALCED + this.holidayUsed);
-        log("LOG__recalcHoliday: total:" + this.holidayTotal + "; used:" + this.holidayUsed + "; left:" + this.holidayLeft);
+        log("LOG recalcHoliday: total:" + this.holidayTotal + "; used:" + this.holidayUsed + "; left:" + this.holidayLeft);
 
       } catch (err) {
-        log("employee.js::Employee::recalcHoliday: " + err);
+        log("ERR employee.js::Employee::recalcHoliday: " + err);
       }
     }
   }
 
-
-
 } catch (err) {
-  log("employee.js: " + err);
+  log("ERR employee.js: " + err);
 }
