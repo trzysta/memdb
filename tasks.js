6e031b4eb1d2d7db4530f0b6b4c322ecf6369e9a
trzysta <61110842+trzysta@users.noreply.github.com>
2020-09-14 10:11:05 +0200

    dt

--- tasks.js
+++ tasks.js
@@ -8,9 +8,13 @@ const Task = function (e) {
 
     this.saveEntry = function () {
 
-      let dtStart = this.entry.field(TAS_FIELD_DATE_START);
-      let weekNr = moment(dtStart).week();
+      const dtStart = moment(this.entry.field(TAS_FIELD_DATE_START)).startOf('week');
+      const dtEnd = moment(dtStart).endOf('week');
+      const weekNr = moment(dtStart).week();
+
       this.entry.set(TAS_FIELD_WEEK, weekNr);
+      this.entry.set(TAS_FIELD_DATE_START, dtStart);
+      this.entry.set(TAS_FIELD_DATE_END, dtEnd));
 
     }
   } catch (err) {
