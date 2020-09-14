e5b7ce73725dfab82b835f54187215313b4b44b5
trzysta <61110842+trzysta@users.noreply.github.com>
2020-09-14 10:48:34 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -9,9 +9,11 @@ const Task = function (e) {
     this.saveEntry = function () {
 
       const dtStart = this.entry.field(TAS_FIELD_DATE_START);
+      const dtEnd = moment(dtStart).add(4, 'days').format();
       const weekNr = moment(dtStart).week();
 
-      this.entry.set(TAS_FIELD_WEEK, weekNr);
+      this.entry.set(TAS_FIELD_WEEK, weekNr)
+      this.entry.set(TAS_FIELD_DATE_END, dtEnd);
 
     }
   } catch (err) {
