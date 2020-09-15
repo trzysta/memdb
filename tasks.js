e77036a0ec25a38da77aa2be0ac4907ca50eb3b2
trzysta <61110842+trzysta@users.noreply.github.com>
2020-09-15 12:56:50 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -20,73 +20,61 @@ const Task = function (e) {
       }
     }
 
+    // this.postSaveEntry = function () {
+
+    //   const dtStart = this.entry.field(TAS_FIELD_DATE_START);
+    //   const dtEnd = moment(dtStart).add(4, 'days').toDate();
+    //   const weekNr = moment(dtStart).week();
+
+    //   let countTotal = 0;
+    //   let countClosed = 0;
+    //   let countRunning = 0;
+    //   let countNotclosed = 0;
+
+    //   this.entry.set(TAS_FIELD_WEEK, weekNr)
+    //   this.entry.set(TAS_FIELD_DATE_END, dtEnd);
+    //   let desc = "zadań: ";
+
+    //   switch (this.status) {
+    //     case TAS_VALUE_WEEKSTATUS_CLOSED:
+
+    //       for (let i = 1; i < 10; i++) {
+    //         if (this.entry.field(TAS_FIELD_TASK + i).length > 0) countTotal += 1;
+    //         if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_CLOSED) { countClosed += 1 };
+    //         if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_RUNNING) { countRunning += 1 };
+    //         if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_NOTCLOSED) { countNotclosed += 1 };
+    //       }
+    //       desc += countTotal + "   (" + countClosed + " wykonane, " + countRunning + " w trakcie, " + countNotclosed + " nie wykonane)";
+    //       break;
+
+    //     default:
+    //       break;
+    //   }
 
 
 
 
+    //   if (this.entry.field(TAS_FIELD_WEEKSTATUS) == TAS_VALUE_WEEKSTATUS_CLOSED) {
 
 
 
-    this.postSaveEntry = function () {
+    //   } else {
+    //     for (let i = 1; i < 10; i++) {
+    //       if (this.entry.field(TAS_FIELD_TASK + i).length > 0) countTotal += 1;
+    //     }
+    //     desc += countTotal;
+    //   }
 
-      const dtStart = this.entry.field(TAS_FIELD_DATE_START);
-      const dtEnd = moment(dtStart).add(4, 'days').toDate();
-      const weekNr = moment(dtStart).week();
+    //   this.entry.set(TAS_FIELD_TASKCOUNT, countTotal);
+    //   this.entry.set(TAS_FIELD_DESCRIPTION, desc);
+    //   this.entry.set(TAS_FIELD_COORDINATOR, this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_COORDINATOR));
 
-      let countTotal = 0;
-      let countClosed = 0;
-      let countRunning = 0;
-      let countNotclosed = 0;
+    //   message(this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_RAPORT_RECIPIENT));
 
-      this.entry.set(TAS_FIELD_WEEK, weekNr)
-      this.entry.set(TAS_FIELD_DATE_END, dtEnd);
-      let desc = "zadań: ";
+    //   this.entry.set(TAS_FIELD_RAPORT_RECIPIENT, this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_RAPORT_RECIPIENT))
+    //   this.entry.recalc();
 
-      switch (this.status) {
-        case TAS_VALUE_WEEKSTATUS_CLOSED:
-
-          for (let i = 1; i < 10; i++) {
-            if (this.entry.field(TAS_FIELD_TASK + i).length > 0) countTotal += 1;
-            if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_CLOSED) { countClosed += 1 };
-            if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_RUNNING) { countRunning += 1 };
-            if (this.entry.field(TAS_FIELD_STATUS + i) == TAS_VALUE_STATUS_NOTCLOSED) { countNotclosed += 1 };
-          }
-          desc += countTotal + "   (" + countClosed + " wykonane, " + countRunning + " w trakcie, " + countNotclosed + " nie wykonane)";
-          break;
-
-        default:
-          break;
-      }
-
-
-
-
-
-
-      if (this.entry.field(TAS_FIELD_WEEKSTATUS) == TAS_VALUE_WEEKSTATUS_CLOSED) {
-
-
-
-      } else {
-        for (let i = 1; i < 10; i++) {
-          if (this.entry.field(TAS_FIELD_TASK + i).length > 0) countTotal += 1;
-        }
-        desc += countTotal;
-      }
-
-      this.entry.set(TAS_FIELD_TASKCOUNT, countTotal);
-      this.entry.set(TAS_FIELD_DESCRIPTION, desc);
-      this.entry.set(TAS_FIELD_COORDINATOR, this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_COORDINATOR));
-
-      message(this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_RAPORT_RECIPIENT));
-
-      this.entry.set(TAS_FIELD_RAPORT_RECIPIENT, this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_RAPORT_RECIPIENT))
-      this.entry.recalc();
-
-    }
-
-
-
+    // }
 
 
 
@@ -101,9 +89,7 @@ const Task = function (e) {
           " ma status: " + this.tasks[i].status + "\n" +
           " opis wykonania: " + this.tasks[i].notes + "\n\n\n\n";
       }
-
       this.entry.field(TAS_FIELD_CONTRACT)[0].field(CON_FIELD_RAPORT_RECIPIENT).sendEmail(subject, body);
-
     }
 
 
