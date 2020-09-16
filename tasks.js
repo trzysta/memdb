6619dfebb0fcb1386ae6abec305dd7fcbdf767ef
trzysta <61110842+trzysta@users.noreply.github.com>
2020-09-16 18:00:12 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -106,12 +106,10 @@ const Task = function (e) {
         this.entry.set(TAS_FIELD_EMAILBODY, body);
         this.entry.set(TAS_FIELD_EMAILSUBJECT, subject);
 
-        let mail = intent("android.intent.action.SENDTO");
-        mail.data("mailto:marceli@trzysta.pl, heniek@trzysta.pl "); // addresses where to send
-        mail.data("android.intent.extra.SUBJECT", subject); // 2nd arg is what you want
-        mail.data("android.intent.extra.TEXT", body); // 2nd arg is what you want
-        // mail.extra("android.intent.extra.ATTACHMENT", "storage/memento/files/myPhoto.jpg"); // THIS IS A GUESS!
-        mail.send(); // send it ou
+        let intentMail = intent("android.intent.action.SENDTO");
+        let act = "mailto:zarzady@trzysta.pl?cc=marceli@trzysta.pl&subject=" + subject + "&body=" + body;
+        intentMail.data(act);
+        intentMail.send(); // send it ou
 
     }
 
