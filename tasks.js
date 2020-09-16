7718c08ff2c8339acbacba4b657420ef0a43398c
trzysta <61110842+trzysta@users.noreply.github.com>
2020-09-16 13:10:19 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -7,6 +7,7 @@ const Task = function (e) {
   try {
 
     this.entry = e;
+
     this.status = this.entry.field(TAS_FIELD_WEEKSTATUS);
     this.dateStart = this.entry.field(TAS_FIELD_DATE_START);
     this.dateEnd = moment(this.dateStart).add(4, 'days').toDate();
@@ -19,72 +20,76 @@ const Task = function (e) {
     for (let i = 1; i < 10; i++) {
 
       if (this.entry.field(TAS_FIELD_TASK + i).length > 0) {
+
         this.tasks[this.tasks.length] = {
+
           content: this.entry.field(TAS_FIELD_TASK + i)
+
         }
+
       }
 
       if (this.entry.field(TAS_FIELD_TASK_PREVWEEK + i).length > 0) {
+
         this.tasksPrevWeek[this.tasksPrevWeek.length] = {
+
           content: this.entry.field(AS_FIELD_TASKPREVWEEK + i),
           status: this.entry.field(TAS_FIELD_STATUSPREVWEEK + i),
           notes: this.entry.field(TAS_FIELD_NOTESPREVWEEK + i)
+
         }
+
       }
 
     }
 
     // * * * * * * * * * * * * * * * * * * * *
     this.closeWeek = function () {
-      try {
 
-        this.entryNextWeek = this.libTasks.linksTo(this.entry)[0];
-        if (this.entryNextWeek === undefined || this.entryNextWeek === null) {
-          this.createNewWeekplan();
-        } else {
-          log(this.entryNextWeek.name + ' ' + this.entry.name);
-          for (let i = 0; i < this.tasks.length; i++) {
-            let n = TAS_FIELD_TASK_PREVWEEK + (i + 1);
-            log(this.entryNextWeek.field(n) + " " + this.tasks[i].content);
-          }
+      this.entryNextWeek = this.libTasks.linksTo(this.entry)[0];
+
+      if (this.entryNextWeek === undefined || this.entryNextWeek === null) {
+
+        this.createNewWeekplan();
+
+      } else {
+
+        log(this.entryNextWeek.name + ' ' + this.entry.name);
+
+        for (let i = 0; i < this.tasks.length; i++) {
+
+          this.entryNextWeek.set(TAS_FIELD_TASK_PREVWEEK + (i + 1), this.tasks[i].content);
+
         }
 
-        this.entryNextWeek.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_RUNNING);
-        this.entry.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_CLOSED);
-
-        this.entryNextWeek.recalc();
-        this.entryNextWeek.show();
-
-
-
-
-      } catch (err) {
-        log("Task::closeWeek:" + err);
       }
-    };
+
+      this.entryNextWeek.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_RUNNING);
+      this.entry.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_CLOSED);
+      this.entryNextWeek.recalc();
+      this.entryNextWeek.show();
+
+    }
+
 
     // * * * * * * * * * * * * * * * * * * * *
     this.createNewWeekplan = function () {
-      try {
 
-        this.entryNextWeek = new Object();
-        this.entryNextWeek = this.libTasks.create(this.entryNextWeek);
-        this.entryNextWeek.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_RUNNING);
-        this.entryNextWeek.set(TAS_FIELD_CONTRACT, this.entryContract);
-        this.entryNextWeek.set(TAS_FIELD_DATE_START, moment(this.dateStart).add(7, 'days').toDate());
-        this.entryNextWeek.set(TAS_FIELD_DATE_END, moment(this.dateStart).add(11, 'days').toDate());
-        this.entryNextWeek.set(TAS_FIELD_WEEK, moment(this.dateStart).add(7, 'days').week());
-        this.entryNextWeek.set(TAS_FIELD_COORDINATOR, this.entry.field(TAS_FIELD_COORDINATOR));
-        this.entryNextWeek.set(TAS_FIELD_TASKCOUNT_PREVWEEK, this.tasks.length);
+      this.entryNextWeek = new Object();
+      this.entryNextWeek = this.libTasks.create(this.entryNextWeek);
+      this.entryNextWeek.set(TAS_FIELD_WEEKSTATUS, TAS_VALUE_WEEKSTATUS_RUNNING);
+      this.entryNextWeek.set(TAS_FIELD_CONTRACT, this.entryContract);
+      this.entryNextWeek.set(TAS_FIELD_DATE_START, moment(this.dateStart).add(7, 'days').toDate());
+      this.entryNextWeek.set(TAS_FIELD_DATE_END, moment(this.dateStart).add(11, 'days').toDate());
+      this.entryNextWeek.set(TAS_FIELD_WEEK, moment(this.dateStart).add(7, 'days').week());
+      this.entryNextWeek.set(TAS_FIELD_COORDINATOR, this.entry.field(TAS_FIELD_COORDINATOR));
+      this.entryNextWeek.set(TAS_FIELD_TASKCOUNT_PREVWEEK, this.tasks.length);
 
-        for (let i = 0; i < this.tasks.length; i++) {
-          this.entryNextWeek.set(TAS_FIELD_TASK_PREVWEEK + (i + 1), this.tasks[i].content);
-        };
-        this.entryNextWeek.link(TAS_FIELD_PREVWEEK, this.entry);
+      for (let i = 0; i < this.tasks.length; i++) {
+        this.entryNextWeek.set(TAS_FIELD_TASK_PREVWEEK + (i + 1), this.tasks[i].content);
+      };
+      this.entryNextWeek.link(TAS_FIELD_PREVWEEK, this.entry);
 
-      } catch (err) {
-        log("Task::createNewWeekplan:" + err);
-      }
     };
 
     // * * * * * * * * * * * * * * * * * * * *
@@ -101,7 +106,9 @@ const Task = function (e) {
           " ma status: " + this.tasks[i].status + "\n" +
           " opis wykonania: " + this.tasks[i].notes + "\n\n\n";
       }
+
       this.entryContract.field(CON_FIELD_RAPORT_RECIPIENT).sendEmail(subject, body);
+
     }
 
     // * * * * * * * * * * * * * * * * * * * *
