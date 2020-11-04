8ebba7382e1862c6a3735a59b01d6403b0043ba9
trzysta <61110842+trzysta@users.noreply.github.com>
2020-11-04 10:35:09 +0100

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,26 +1,24 @@
 
-const FIELD_LABEL = "1) Czynność";
-const FIELD_PROMPT = "1) Cechy";
+const FIELD_LABEL = ") Czynność";
+const FIELD_PROMPT = ") Cechy";
 
 
-let actions = new Array;
+// var entries = lib().entries();                   // Get current library & array of its entries
+// if (entries.length > 0) {                        // Check that array is not empty;
+//   //   otherwise exit,
+//   //   since there is no previous entry.
+//   prevMileage = entries[0].field("Mileage");
 
-actions[0] = {
-  label: "Posadzka, płytki, kamień",
-  prompt: [
-    "Ślady zamazów na płytkach",
-    "Piach",
-    "Niedokładne mopowanie",
-    "Niedomyte w rogach"
-  ]
-}
+const copyLastValues = function (e) {
 
+  let allEntries = lib().entries();
 
-
-
-const setValues = function (e) {
-
-  e.set(FIELD_LABEL, actions[0].label);
-  e.set(FIELD_PROMPT, actions[0].prompt);
+  if (allEntries.length > 0) {
+    let lastEntry = allEntries[0];
+    for (i = 1; i <= 10; i++) {
+      e.set(i + FIELD_LABEL, lastEntry.field(i + FIELD_LABEL));
+      e.set(i + FIELD_PROMPT, lastEntry.field(i + FIELD_PROMPT));
+    }
+  }
 
 } 
