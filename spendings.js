a6e9e4fc449f0028ef44168c181b2bda1a06cc3f
trzysta <marceli@trzysta.pl>
2022-04-21 16:01:24 +0200

    spendings

--- spendings.js
+++ spendings.js
@@ -1,12 +1,42 @@
+
+// *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
+
+const SPE_VALUE_TYPE_1 = "Zakup za gotówkę";
+const SPE_VALUE_TYPE_2 = "Zakup z karty";
+const SPE_VALUE_TYPE_3 = "Zakup na przelew";
+const SPE_VALUE_TYPE_4 = "Wypłacona gotówką zaliczka";
+const SPE_VALUE_TYPE_5 = "Rozliczenie gotówką z pracownikiem";
+const SPE_VALUE_TYPE_6 = "Wypłacona przelewem zaliczka";
+const SPE_VALUE_TYPE_7 = "Rozliczenie przelewem z pracownikiem";
+const SPE_VALUE_TYPE_8 = "Przekazanie gotówki koordynatorowi";
+const SPE_VALUE_TYPE_9 = "Wypłata z bankomatu";
+const SPE_VALUE_ISPAYED_TRUE = "Zapłacone";
+
+const DROP_NULL_PLACEHOLDER = "- wybierz -";
+const SPE_PURCHASES = [SPE_VALUE_TYPE_1, SPE_VALUE_TYPE_2, SPE_VALUE_TYPE_3];
+const SPE_SALARIES  = [SPE_VALUE_TYPE_4, SPE_VALUE_TYPE_5, SPE_VALUE_TYPE_6, SPE_VALUE_TYPE_7];
+const SPE_CASH = [SPE_VALUE_TYPE_8, SPE_VALUE_TYPE_9];
+
+const SPE_FIELD_AMOUNT = "Kwota";
+const SPE_FIELD_PAYER = "Dokonujący transakcji";
+const SPE_FIELD_RECIPIENT = "Przekazano osobie";
+const SPE_FIELD_TRANSTYPE = "Typ transakcji";
+const SPE_FIELD_CATEGORY = "Kategoria";
+const SPE_FIELD_PAYMENTDUE = "Termin płatności";
+const SPE_FILED_DATEPAYED = "Data dokonania zapłaty";
+const SPE_FILED_DATETRANSACTION = "Data transakcji";
+const SPE_FIELD_ISPAYED = "Do zapłaty";
+const SPE_FIELD_NR = "Nr";
+
+const SPE_FIELD_NRLINKED = "Nr powiązany";
+const SPE_FIELD_DESC = "Opis";
+const SPE_FIELD_LINK = "Link";
+
+const FIELD_EDITORS = "Editor";
+
+
 // *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 // *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
-// *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
-
-
-
-
-
-
 
 
 const ev_new_przedZapisem = function ( e ) {
@@ -17,107 +47,86 @@ const ev_new_przedZapisem = function ( e ) {
   od rodaju wydatku
 */
 
-var nll = "- wybierz -";
-var wyd = ["Zakup za gotówkę", "Zakup z karty", "Zakup na przelew"];
-var lib = libByName("Wydatki");
-var e = entry();
-var s = e.field("Kwota");
-var p = e.field("Dokonujący transakcji");
-var k = e.field("Przekazano osobie");
-var t = e.field("Typ transakcji");
-var kat = e.field("Kategoria");
-var editors = ["MalgorzataG", "ElzbietaZ", "trzystaIZABELA", "trzysta"];
+let libSpendings = libByName( LIB_SPANDINGS_NAME );
+let amount = e.field(SPE_FIELD_AMOUNT);
+let transactionType = e.field(SPE_FIELD_TRANSTYPE);
 
-e.set("Editor", editors);
-    
-if (t == "Przekazanie gotówki koordynatorowi" && k == p) {
+if (transactionType == SPE_VALUE_TYPE_8 && e.field(SPE_FIELD_RECIPIENT) == e.field(SPE_FIELD_PAYER)) {
   message("Nie można przekazać tej samej osobie");
   cancel();
-} else if (p == nll) {
+} else if (e.field(SPE_FIELD_PAYER) == DROP_NULL_PLACEHOLDER) {
   message("Wybierz z listy osobę dokonującą transakcji");
   cancel();
-} else if (t == "Przekazanie gotówki koordynatorowi" && k == nll) {
+} else if (transactionType == SPE_VALUE_TYPE_8 && e.field(SPE_FIELD_RECIPIENT) == DROP_NULL_PLACEHOLDER) {
   message("Wybierz komu przekazano gotówkę!");
   cancel();
-} else if ( wyd.indexOf(t) >= 0 &&  kat == nll ) {
+} else if ( SPE_PURCHASES.indexOf(transactionType) >= 0 && e.field(SPE_FIELD_CATEGORY) == DROP_NULL_PLACEHOLDER ) {
   message("Wybierz kategorię wydatku");
   cancel();
 } else {
 
   // operacje po walidacji
 
-
-
-  switch (t) {
-    case "Zakup z karty":
-    case "Zakup za gotówkę":
-    case "Wypłacona gotówką zaliczka":
-    case "Rozliczenie gotówką z pracownikiem":
-    case "Rozliczenie przelewem z pracownikiem":
-    case "Wypłacona przelewem zaliczka":
-      e.set("Termin płatności", "");
-      e.set("Data dokonania zapłaty", e.field("Data transakcji"));
-      e.set("Do zapłaty", "Zapłacone");
-      e.set("Kwota", 0 - Math.abs(s));
-      if (
-          (t == "Rozliczenie gotówką z pracownikiem") || 
-          (t == "Wypłacona gotówką zaliczka") || 
-          (t == "Rozliczenie przelewem z pracownikiem") || 
-          (t == "Wypłacona przelewem zaliczka")
-         ) {
-             e.set("Kategoria", "Wynagrodzenia") 
-          }
+  switch (transactionType) {
+    case SPE_VALUE_TYPE_2:
+    case SPE_VALUE_TYPE_1:
+    case SPE_VALUE_TYPE_4:
+    case SPE_VALUE_TYPE_5:
+    case SPE_VALUE_TYPE_7:
+    case SPE_VALUE_TYPE_6:
+      e.set(SPE_FIELD_PAYMENTDUE, "");
+      e.set(SPE_FILED_DATEPAYED, e.field(SPE_FILED_DATETRANSACTION));
+      e.set(SPE_FIELD_ISPAYED, SPE_VALUE_ISPAYED_TRUE);
+      e.set(SPE_FIELD_AMOUNT, 0 - Math.abs(amount));
+      //if (SPE_SALARIES.indexOf(transactionType) >= 0) e.set(SPE_FIELD_CATEGORY, "Wynagrodzenia") 
+      break;
+    
+    case SPE_VALUE_TYPE_3:
+      e.set(SPE_FIELD_AMOUNT, 0 - Math.abs(amount));
       break;
 
-    case "":
-    case "Zakup na przelew":
-      e.set("Kwota", 0 - Math.abs(s));
-      break;
+    case SPE_VALUE_TYPE_8:
 
-    case "Przekazanie gotówki koordynatorowi":
-
-      log("Przekazanie gotówki koordynatorowi: ");
+      log(SPE_VALUE_TYPE_8);
 
       try {
 
         var newE = new Object();
-        var newEntry;
         var newNr;
         var sRes;
 
-        e.set("Kategoria", "");
-        e.set("Termin płatności", "");
-        e.set("Kwota", 0 - Math.abs(s));
-        e.set("Data dokonania zapłaty", e.field("Data transakcji"));
-        e.set("Do zapłaty", "Zapłacone");
+        e.set(SPE_FIELD_CATEGORY, "");
+        e.set(SPE_FIELD_PAYMENTDUE, "");
+        e.set(SPE_FIELD_AMOUNT, 0 - Math.abs(amount));
+        e.set(SPE_FILED_DATEPAYED, e.field(SPE_FILED_DATETRANSACTION));
+        e.set(SPE_FIELD_ISPAYED, SPE_VALUE_ISPAYED_TRUE);
 
         newNr = guid().toUpperCase();//Math.floor(10000 + (Math.random() * 90000));
-        sRes = lib.find(newNr);
+        sRes = libSpendings.find(newNr);
 
         while (sRes.length != 0) {
             newNr = guid().toUpperCase();//Math.floor(10000 + (Math.random() * 90000));
-            sRes = lib.find(newNr);
+            sRes = libSpendings.find(newNr);
         };
 
-        e.set("Nr powiązany", newNr);
-        e.set("Link", null);
+        e.set(SPE_FIELD_NRLINKED, newNr);
+        e.set(SPE_FIELD_LINK, null);
 
-        newE["Typ transakcji"] = e.field("Typ transakcji");
-        newE["Opis"] = e.field("Opis");
-        newE["Do zapłaty"] = "Zapłacone";
-        newE["Data dokonania zapłaty"] = e.field("Data transakcji");
-        newE["Data transakcji"] = e.field("Data transakcji");
-        newE["Dokonujący transakcji"] = e.field("Przekazano osobie");
-        newE["Przekazano osobie"] = e.field("Dokonujący transakcji");
-        newE["Editor"] = e.field("Editor");
-        newE["Opis"] = e.field("Opis");
-        newE["Kwota"] = Math.abs(s);
-        newE["Nr"] = newNr;
-        newE["Nr powiązany"] = e.field("Nr");
-        newE["Kategoria", ""];
+        newE[SPE_FIELD_TRANSTYPE] = e.field(SPE_FIELD_TRANSTYPE);
+        newE[SPE_FIELD_DESC] = e.field(SPE_FIELD_DESC);
+        newE[SPE_FIELD_ISPAYED] = SPE_VALUE_ISPAYED_TRUE;
+        newE[SPE_FILED_DATEPAYED] = e.field(SPE_FILED_DATETRANSACTION);
+        newE[SPE_FILED_DATETRANSACTION] = e.field(SPE_FILED_DATETRANSACTION);
+        newE[SPE_FIELD_PAYER] = e.field(SPE_FIELD_RECIPIENT);
+        newE[SPE_FIELD_RECIPIENT] = e.field(SPE_FIELD_PAYER);
+        newE[SPE_FIELD_DESC] = e.field(SPE_FIELD_DESC);
+        newE[SPE_FIELD_AMOUNT] = Math.abs(amount);
+        newE[SPE_FIELD_NR] = newNr;
+        newE[SPE_FIELD_NRLINKED] = e.field(SPE_FIELD_NR);
+        newE[SPE_FIELD_CATEGORY, ""];
 
-        newE = lib.create(newE);
-        newE.link("Link", e);
+        newE = libSpendings.create(newE);
+        newE.link(SPE_FIELD_LINK, e);
 
         log("Przekazanie gotówki koordynatorowi: koniec");
         //e.recalc();
@@ -128,12 +137,12 @@ if (t == "Przekazanie gotówki koordynatorowi" && k == p) {
 
       break;
 
-    case "Wypłata z bankomatu":
-      e.set("Termin płatności", "");
-      e.set("Data dokonania zapłaty", e.field("Data transakcji"));
-      e.set("Do zapłaty", "Zapłacone");
-      e.set("Kwota", Math.abs(s));
-      e.set("Kategoria", "");
+    case SPE_VALUE_TYPE_9:
+      e.set(SPE_FIELD_PAYMENTDUE, "");
+      e.set(SPE_FILED_DATEPAYED, e.field(SPE_FILED_DATETRANSACTION));
+      e.set(SPE_FIELD_ISPAYED, SPE_VALUE_ISPAYED_TRUE);
+      e.set(SPE_FIELD_AMOUNT, Math.abs(amount));
+      e.set(SPE_FIELD_CATEGORY, "");
       break;
   }
 }
@@ -161,7 +170,6 @@ if (t == "Przekazanie gotówki koordynatorowi" && k == p) {
 // 1. Wszystkie pozycje puste = podzial równo po wszystkie pozycje.
 // 2. kwota przypisania mniejsza i istnieją puste. Odejmij od kwoty to co przypisane a resztę podziel.
 
-const FIELD_EDITORS = "Editor";
 
 
 const Spending = function (e) {
