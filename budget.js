4426d3a9e43fc10d76e15b4bd134a90216bd6e35
trzysta <marceli@trzysta.pl>
2022-04-28 14:16:00 +0200

    bud

--- budget.js
+++ budget.js
@@ -9,7 +9,7 @@ const assignSpendingToBudget = function (entryBudget, entrySpending) {
 
   try {
     
-
+    log("start while");
     while (budgetLinkNr > entrySpending.field(SPE_FIELD_BUDGET_LINK).lenght && !isFound ) {
       if ( entryBudget.id == entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].id) {
         isFound = true;
@@ -18,7 +18,8 @@ const assignSpendingToBudget = function (entryBudget, entrySpending) {
         log(budgetLinkNr + " not found: " + entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].id)
         budgetLinkNr++;
       }
-    }
+    };
+    log("staend while " + budgetLinkNr);
 
     currentCategoryName = entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].attr( BUD_FIELD_LINK_ATTR_CATEGORY );
     currentAmount = entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].attr( BUD_FIELD_LINK_ATTR_AMOUNT );
