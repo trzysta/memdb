592e63af9e06945f53cc3a8ee42a32f36c26cbb0
trzysta <marceli@trzysta.pl>
2022-04-28 14:24:52 +0200

    bud

--- budget.js
+++ budget.js
@@ -9,8 +9,8 @@ const assignSpendingToBudget = function (entryBudget, entrySpending) {
 
   try {
     
-    log("start while" + entrySpending.field(SPE_FIELD_BUDGET_LINK).lenght );
-    
+    log("start while " + entrySpending.field(SPE_FIELD_BUDGET_LINK).lenght );
+
     while (budgetLinkNr < entrySpending.field(SPE_FIELD_BUDGET_LINK).lenght && !isFound ) {
       if ( entryBudget.id == entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].id) {
         isFound = true;
@@ -25,7 +25,13 @@ const assignSpendingToBudget = function (entryBudget, entrySpending) {
     currentCategoryName = entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].attr( BUD_FIELD_LINK_ATTR_CATEGORY );
     currentAmount = entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].attr( BUD_FIELD_LINK_ATTR_AMOUNT );
     prevCategoryAmount = entrySpending.field(SPE_FIELD_BUDGET_LINK)[budgetLinkNr].field( currentCategoryName );
-    entryBudget.set( thisCategoryName, prevCategoryAmount + thisAmount );
+
+    log (
+      "currentCategoryName: " + currentCategoryName + ", " +
+      "currentAmount: " + currentAmount + ", " +
+      "prevCategoryAmount: " + prevCategoryAmount 
+    )
+    entryBudget.set( currentCategoryName, prevCategoryAmount + thisAmount );
 
   } catch (error) {
     log('ERR: Budget :: assignSpendingToBudget :: ' + error);
