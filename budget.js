4c5d265e2da7b07b8c36c3a8901fb43a0c437530
trzysta <marceli@trzysta.pl>
2022-06-09 12:57:28 +0200

    updateBudget

--- budget.js
+++ budget.js
@@ -1,16 +1,54 @@
-const BUD_FIELD_PROJECTION_TYPE = "Rodzaj wpisu";
-const BUD_FIELD_PROJECTION_CATEGORY = "Kategoria";
+const BUD_FIELD_PROJECTION_INCOMEORSPEND = "Koszt czy wydatek";
+const BUD_FIELD_ENTRY_TYPE = "Rodzaj wpisu";
+
+const BUD_VALUE_ENTRY_TYPE_BUDGET = "Budżet";
+const BUD_VALUE_ENTRY_TYPE_PROJECTION = "Pozycja w budżecie";
 
 const BUD_PROJECTION_TYPE_INCOME = "Przychód";
 const BUD_PROJECTION_TYPE_SPEND = "Koszt"
 
 const BUD_FIELD_BUDGET_NAME = "Budżet";
 
-const BUD_FIELD_BUDGET_LINKED_ENTRY = "Definicja nadrzędna";
+const BUD_FIELD_LINKEDPROJECTIONS = "Powiązane pozycje budżetowe";
 
 const BUD_FIELD_PROJECTION_AMOUNT = "Kwota"
 const BUD_FIELD_BUDGET_INCOME_AMOUNT = "Przychody";
 const BUD_FIELD_BUDGET_SPEND_AMOUNT = "Wydatki";
+
+
+
+const updateBudget = function (entryBudget) {
+ 
+  log( 'Budget :: updateBudget:' + entryBudget.name);
+  if (entryBudget.field(BUD_FIELD_ENTRY_TYPE) == BUD_VALUE_ENTRY_TYPE_BUDGET ) {
+
+    let incomeTotal = 0;
+    let spendTotal = 0;
+    
+    let links = entryBudget.field( BUD_FIELD_LINKEDPROJECTIONS ).length;
+    
+    for ( i=0; i<links; i++ ) {
+
+      let e = entryBudget.field( BUD_FIELD_LINKEDPROJECTIONS )[i];
+      log( 'Budget :: updateBudget:' + entryBudget.name + " for " + i + ", " + e().name );
+
+      if (e.field(BUD_FIELD_PROJECTION_INCOMEORSPEND) == BUD_PROJECTION_TYPE_INCOME ) {
+        incomeTotal = incomeTotal + e.field( BUD_FIELD_PROJECTION_AMOUNT )
+
+      } else {
+        spendTotal = spendTotal + e.field( BBUD_PROJECTION_TYPE_SPEND )
+        
+      }
+    }
+    entryBudget.set( BUD_FIELD_BUDGET_INCOME_AMOUNT, incomeTotal );
+    entryBudget.set( BUD_FIELD_BUDGET_SPEND_AMOUNT, spendTotal );
+  }
+}
+
+
+
+
+
 
 const saveProjection = function (entryProjection, isNew) {
   
@@ -28,8 +66,10 @@ const saveProjection = function (entryProjection, isNew) {
   
       let prevBudgetAmount = entryBudget.field( BUD_FIELD_BUDGET_INCOME_AMOUNT );
       let prevBudgetCategoryAmount = entryBudget.field( projectionCategoryName );
+      
       entryBudget.set( BUD_FIELD_BUDGET_INCOME_AMOUNT, prevBudgetAmount + projectionAmount );
       entryBudget.set( projectionCategoryName, prevBudgetCategoryAmount + projectionAmount );
+
       log ('Budget :: saveProjection - INCOME: ' + projectionCategoryName + " prev:" + prevBudgetCategoryAmount + " totalPrev: " + prevBudgetAmount + " adding: " + projectionAmount);
   
     } else {
